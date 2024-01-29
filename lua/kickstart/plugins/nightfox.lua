d9732695a1b110f8a0ddc4c38c0be401853ee113
Joar <joar.sabel@netikka.fi>
2024-01-30 00:43:17 +0100

    Change theme, add MarkdownPreview and Noice.nvim
    Changed theme to Nightfox - Nordfox variation. Added markdownPreview
    :goat_emoji:. Finally added base for Noice.nvim, still gotta figure out
    how to disable some of those notifications, they drive me wild

---
+++ lua/kickstart/plugins/nightfox.lua
@@ -0,0 +1,20 @@
+-- WIP
+return {
+  {
+    "EdenEast/nightfox.nvim",
+    lazy=false,
+    priority = 1000,
+    opts = {
+      options = {
+        transparent = true,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        }
+      }
+    }
+
+  }
+
+}
