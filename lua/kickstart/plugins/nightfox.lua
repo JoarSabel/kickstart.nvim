f63f6a174d0c474c61548084d23f33236647d686
joar <joar.sabel@netikka.fi>
2024-08-12 20:52:56 +0300

    Un-transparentarize the theme, I've done did had it

--- lua/kickstart/plugins/nightfox.lua
+++ lua/kickstart/plugins/nightfox.lua
@@ -1,29 +1,27 @@
 -- WIP
 return {
   {
-    "EdenEast/nightfox.nvim",
-    lazy=false,
+    'EdenEast/nightfox.nvim',
+    lazy = false,
     priority = 1000,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'nordfox'
+      vim.cmd.colorscheme 'duskfox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
     opts = {
       options = {
-        transparent = true,
+        transparent = false,
         styles = {
-          comments = "italic",
-          keywords = "bold",
-          types = "italic,bold",
-        }
-      }
-    }
-
-  }
-
+          comments = 'italic',
+          keywords = 'bold',
+          types = 'italic,bold',
+        },
+      },
+    },
+  },
 }
